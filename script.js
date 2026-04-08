2b9472dd7e245678dc392c4a9d84d335572f58de
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-04-08 19:24:08 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-  document.getElementById("form-message").style.color = "green";
-  document.getElementById("form-message").textContent =
-    "Thank you for contacting us! We'll get back to you soon.";
-  this.reset();
-});
-
